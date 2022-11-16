9f7f8c7678501ed39abe328be2d9d786b63e5e4a
Nacho <nachote01@gmail.com>
2022-11-16 23:15:32 +0100

    ValueError: ried to insert duplicated variable '1'

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ lines = open('students.txt', 'r').read().splitlines()
 students = [elem.split(',') for elem in lines]
 
 domains =[
-    list(elem) for elem in 
-    list( it.product(list(range(1,NUMSEATS+1)), students) )
+    [a] + b for a in range(1, NUMSEATS+1) for b in students
 ]
     # cartesian product (each seat possibility) x (each student)
 
 
 vars = [str(i) for i in range(1,NUMSEATS+1)]
 print(vars)
+print(domains[0:32])
 
-problem = constraint.Problem(vars, domains)
+problem = constraint.Problem()
+problem.addVariables('1', domains[0:31])
+for i, elem in enumerate(vars):
+    problem.addVariables(elem, str(domains[(i-1)*32:i*32]))
+
+
