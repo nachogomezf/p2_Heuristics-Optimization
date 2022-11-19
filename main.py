130432cb3e9cd738ecc8af8f2f2e01e078e69dc4
Nacho <nachote01@gmail.com>
2022-11-19 13:55:26 +0100

    casi-working. mira ult coment

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from constraint import *
 import re
 import numpy as np
 
-NUMSEATS = 32
 lines = open('students2.txt', 'r').read().splitlines()
 students = [elem.split(',') for elem in lines]
 
@@ -45,6 +44,18 @@ def studentAt(list, val):
         if st[0] == val:
             return index
 
+def areSiblings(st1, st2):
+    for st in siblings.items():
+        if st == (st1,st2) or st == (st2,st1):
+            return True
+        else: return False
+
+def seatPos(seat):
+    res = []
+    for elem in np.where(bus == seat):
+        res.append(elem[0])
+    return res
+
 fulldomain = [i for i in range(1,33)]
 
 domain1yr = [i for i in range(5,13)]
@@ -54,66 +65,68 @@ domain2yrC = [i for i in range(17,21)]
 domain2yr = [i for i in range(21,33)]
 domain1yrC = [i for i in range(1,5)] + [i for i in range(13,17)]
 
-
+studentsDict = {st[0] : st for st in students}
 allstudents = [st[0] for st in students]
 
 students1yr = [st[0] for st in students if st[1] == '1']
 students2yr = [st[0] for st in students if st[1] == '2']
 
-siblings12yr = [[st[0], st[4]] for st in students if st[1] == '1' and st[4] in students2yr]
+'''siblings12R = [[st[0] , st[4]] for st in students if st[1] == '1' and st[4] in students2yr and (st[3] == 'R' or studentsDict[st[4]][3] == 'R')]
+siblings12yr = [[st[0], st[4]] for st in students if st[1] == '1' and st[4] in students2yr and st[3] != 'R' and studentsDict[st[4]][3] != 'R']
+
 siblings12yr = sum(siblings12yr, [])
-studentsNoSiblings = [st for st in students if st[0] not in siblings12yr]
+siblings12R = sum(siblings12R, [])
+studentsNoSiblings = [st for st in students if st[0] not in siblings12yr and st[0] not in siblings12R]
 
 students1yr = removeFrom(students1yr, siblings12yr)
-students2yr = removeFrom(students2yr, siblings12yr)
+students1yr = removeFrom(students1yr, siblings12R)
 
+students2yr = removeFrom(students2yr, siblings12R)
+students2yr = removeFrom(students2yr, siblings12yr)
+'''
+
+for index, st in enumerate(students):
+    if st[1] == '2' and st[4] in students1yr:
+        students[index][1] = '1'
+        #change the second year to student to be first year internally if they are siblings from different years
+print(students)
 studentsCX = [st[0] for st in students if st[2] == 'C' and st[3] == 'X']
 studentsCR = [st[0] for st in students if st[2] == 'C' and st[3] == 'R']
 studentsXR = [st[0] for st in students if st[2] == 'X' and st[3] == 'R']
 studentsXX =  [st[0] for st in students if st[2] == 'X' and st[3] == 'X']
 
-students1XX = [st[0] for st in studentsNoSiblings if st[1] == '1' and st[2] == 'X' and st[3] == 'X']
-students1CX = [st[0] for st in studentsNoSiblings if st[1] == '1' and st[2] == 'C' and st[3] == 'X']
-students1XR = [st[0] for st in studentsNoSiblings if st[1] == '1' and st[2] == 'X' and st[3] == 'R']
-students1CR = [st[0] for st in studentsNoSiblings if st[1] == '1' and st[2] == 'C' and st[3] == 'R']
+students1XX = [st[0] for st in students if st[1] == '1' and st[2] == 'X' and st[3] == 'X']
+students1CX = [st[0] for st in students if st[1] == '1' and st[2] == 'C' and st[3] == 'X']
+students1XR = [st[0] for st in students if st[1] == '1' and st[2] == 'X' and st[3] == 'R']
+students1CR = [st[0] for st in students if st[1] == '1' and st[2] == 'C' and st[3] == 'R']
 
-students2XX = [st[0] for st in studentsNoSiblings if st[1] == '2' and st[2] == 'X' and st[3] == 'X']
-students2CX = [st[0] for st in studentsNoSiblings if st[1] == '2' and st[2] == 'C' and st[3] == 'X']
-students2XR = [st[0] for st in studentsNoSiblings if st[1] == '2' and st[2] == 'X' and st[3] == 'R']
-students2CR = [st[0] for st in studentsNoSiblings if st[1] == '2' and st[2] == 'C' and st[3] == 'R']
+students2XX = [st[0] for st in students if st[1] == '2' and st[2] == 'X' and st[3] == 'X']
+students2CX = [st[0] for st in students if st[1] == '2' and st[2] == 'C' and st[3] == 'X']
+students2XR = [st[0] for st in students if st[1] == '2' and st[2] == 'X' and st[3] == 'R']
+students2CR = [st[0] for st in students if st[1] == '2' and st[2] == 'C' and st[3] == 'R']
 
 
 
 problem = Problem()
-problem.addVariables(siblings12yr, domain1yrwithC)
+# problem.addVariables(siblings12yr, domain1yrwithC)
 problem.addVariables(students1XX + students1CX, domain1yrwithC)
 problem.addVariables(students2XX + students2CX, domain2yrC + domain2yr)
 problem.addVariables(students1XR + students1CR, domain1yrC)
 problem.addVariables(students2XR + students2CR, domain2yrC)
 
-
-
 problem.addConstraint(AllDifferentConstraint())
 
-# For troublemaking students
-    # For troublemaking and disabled students
-        #if they are not the same then the seat of the student cannot be a neighbour of the other troublemaker or disabled
+# Dictionary of siblings, with the key being the younger brother always
+siblings = {st[0] : st[4] for st in students if st[4] > '0' and st[0] <= students[studentAt(students, st[4])][0]}
 
-def areSiblings(st1, st2):
-    for st in students:
-        if st[0] == st1:
-            if st[4] == st2:
-                return True
-    return False
-siblings= [[st[0], st[4]] for st in students if st[4] > '0' and st[0] <= students[studentAt(students, st[4])][0] ]
-
-
-for pair in siblings: 
+# SIBLINGS RESTRICTION
+for sib in siblings.items(): 
     problem.addConstraint(
         lambda young, old: (young % 4 == 1 and old == young+1) or (young % 4 == 0 and young == old +1),
-        (pair[0], pair[1])
+        (sib[0], sib[1])
     )
 
+#REDUCED MOBILITY RESTRICTION
 
 for st1 in studentsXR+studentsCR:
     for st2 in studentsXR+studentsCR:
@@ -124,36 +137,21 @@ for st1 in studentsXR+studentsCR:
                                 ) #next seat to reduced mobility must be free
 
 
-
-
-def seatPos(seat):
-    res = []
-    for elem in np.where(bus == seat):
-        res.append(elem[0])
-    return res
+#TROUBLESOME STUDENTS RESTRICTION
 
 def trouble(seat1, seat2):
-    print("a", seat1, seat2)
     if seat2 not in neighbors(bus, seatPos(seat1)[0], seatPos(seat1)[1]):
         print(seat1, neighbors(bus, seatPos(seat1)[0], seatPos(seat1)[1]), seat2)
         return True
 
 for stC in studentsCR + studentsCX: #troublesome students
     for st2 in studentsCR + studentsCX + studentsXR: #troublesome and disabled students
-        if stC != st2:
+        if stC != st2 and not areSiblings(stC, st2):
             problem.addConstraint(
             trouble
             ,(stC, st2)
             )
 
+#What happens if one is brother is troubled and the other is disabled??? Se sientan juntos con esta version... not sure si esta bien
+
 print(problem.getSolution())
-
-
-'''If two students are siblings they must be seated next to each other. For example, siblings could sit on seats
-5 and 6 but not on 6 and 7 (since they would be separated by the aisle).
-If siblings are on different years, then an exception would be made to the previous rule (front for first year, back for second year):
-both would be placed in the front, and the older brother has to be assigned the seat closer to the aisle. 
-If both brothers are troublesome they still have to sit together, but rules regarding other troublesome students
-still apply. If one sibling has reduced mobility they do not have to be seated together, but they still have
-to be seated in the same section (front or back, depending on the case and following previous rules).
-'''
