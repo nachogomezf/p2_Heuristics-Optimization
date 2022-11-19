7f6b027997f8ef053f2ebc2241dfb5f2ca9d6daa
Nacho <nachote01@gmail.com>
2022-11-20 00:15:21 +0100

    calls.sh

--- part-1/calls.sh
+++ part-1/calls.sh
@@ -1,1 +1,7 @@
+python3 CSPBusSeats.py CSP-tests/students_7disabled.txt
+python3 CSPBusSeats.py CSP-tests/students_tests.txt
+python3 CSPBusSeats.py CSP-tests/students2.txt
 python3 CSPBusSeats.py CSP-tests/students3.txt
+python3 CSPBusSeats.py CSP-tests/students_8troubled.txt
+python3 CSPBusSeats.py CSP-tests/students_9troubled.txt
+python3 CSPBusSeats.py CSP-tests/students33people.txt
