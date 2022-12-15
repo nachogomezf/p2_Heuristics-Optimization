1a3be3e597828fccf2825dbb53529c83e1cd9b2d
Nacho <nachote01@gmail.com>
2022-12-15 15:45:15 +0100

    fallo output

---
+++ part-2/ASTAR-calls.sh
@@ -0,0 +1,2 @@
+python3 AStarBusQ.py easytest.prob 1
+python3 AStarBusQ.py students.prob 1
--- part-2/AStarBusQ.py
+++ part-2/AStarBusQ.py
@@ -1,112 +1,109 @@
 import ast
-
+import time
+import sys
+import os
 
 class Node:
-    def __init__(self, student, seat):
-        self.id =  student[:-2]
-        self.disabled = True if student[-1] == 'R' else False
-        self.troubled = True if student[-2] == 'C' else False
-        self.seat = seat
+    def __init__(self, students: list =[], remaining: list =[]):
+        self.state = students
+        self.remaining = remaining
+        self.cost = 0
+        if students:
+            # if there was at least a student in the queue, check if it was reduced mobility
+            if students[0].rm:
+                self.cost = 3
+            else:
+                self.cost = 1
+        self.heuristic = len(remaining) if sys.argv[2] == '1' else 0
+        prev = None
+        tr_seats = []
+        for i in students:
+            if i.tr:
+                tr_seats.append(i.seat)
+            for elem in tr_seats:
+                if i.seat > elem:
+                    if i.tr: self.cost += 3
+                    else: self.cost += 1
+            if prev:
+                if not prev.rm:
+                    if prev.tr:
+                        if i.rm: self.cost += 6
+                        else: self.cost += 2
+                    # not trouble
+                    else:
+                        if i.rm: self.cost += 3
+                        else: self.cost += 1
+                else:
+                    if i.tr:
+                        self.cost += 3
+            prev = i
         self.parent = None
-        self.H = 0
-        self.G = 3 if self.disabled else 1
-    
-    def cost(self,other):
-        return 3 if self.disabled else 1
-        
-def children(node, students):
-    if node is None: return students #base case
-    result = []
-    for st1 in students:
-        #If the student isn't the current one, add it to the children list
-        if st1.id != node.id: 
-            st1.parent = node
-            result.append(st1)
-    return result
+        if self.parent:
+            self.cost += self.parent.cost
+        self.totalcost = self.cost + self.heuristic
 
-    
+class Student:
+    def __init__(self, id, seat, tr, rm) -> None:
+        self.id = id
+        self.seat = seat
+        self.tr = True if tr == 'C' else False
+        self.rm = True if rm == 'R' else False
 
-def check_goal(current, closedset, students):
-    if current.id in [o.id for o in closedset]: return False
-    return True if (len(students) == (1+len(closedset))) else False
-
-def addStudents(students):
+def getStudents(students):
     res = []
-    for st, seat in students.items():
-        res.append(
-            Node(st, seat)
-        )
+    for i, j in students.items():
+        res.append(Student(i[:-2], j, i[-2], i[-1]))
     return res
 
-def currentQueueLength(current):
-    path = []
-    while current.parent:
-        path.append(current)
-        current = current.parent
-    return len(path)
+def children(node):
+    res = []
+    # Mete los operadores y restricciones aqui
+    
+    for i in node.remaining:
+        if node.state:
+            if node.state[-1].rm and i.rm:
+                # if prev and current are R, do not generate the child since we cannot have two consecutive disabled students
+                continue
+        mylist = node.remaining.copy()
+        mylist.remove(i)
+        res.append(Node(node.state + [i], mylist))
+    return res
 
-def aStar(students):
-    #The open and closed sets
+
+def astar(students):
     openset = set()
     closedset = set()
-    #Get all the students as operators
-    current = children(None, students)
-    #Add the starting node to the open set by selecting the one with lesser cost
-    for st in current:
-        openset.add(st)
-    #While the open set is not empty
+    current = Node([], students)
+    openset.add(current)
+
     while openset:
-        #Find the item in the open set with the lowest G + H score
-        current = min(openset, key=lambda o:o.G + o.H)
-        #If it is the item we want, retrace the path and return it
-        if currentQueueLength(current) == len(students)-1:
-            path = []
-            while current.parent:
-                path.append(current)
-                current = current.parent
-            path.append(current)
-            return path[::-1]
-        #Remove the item from the open set
+        current = min(openset-closedset, key=lambda i : i.totalcost)
+        if current.state and (not current.remaining) and current.state[-1] != 'R': return openset, closedset, current
         openset.remove(current)
-        #Add it to the closed set
         closedset.add(current)
-        #Loop through the node's children
-        for node in children(current, students):
-            #If it is already in the closed set, skip it
-            if node in closedset:
-                continue
-            #We cannot have two successive disabled people
-            if current.disabled and node.disabled:
-                continue
-            #Falta checkear si es disabled y es el ultimo
-            #Otherwise if it is already in the open set
-            if node in openset:
-                #Check if we beat the G score 
-                new_g = current.G + current.cost(node)
-                if node.G >= new_g:
-                    #If so, update the node to have a new parent
-                    node.G = new_g
-                    node.parent = current
-            else:
-                #If it isn't in the open set, calculate the G and H score for the node
-                node.G = current.G + current.move_cost(node)
-                # TODO : comprobar heuristica es admisible
-                node.H = len(students) - len(closedset)
-                #Set the parent to our current item
-                node.parent = current
-                #Add it to the set
-                openset.add(node)
-    #Throw an exception if there is no path
+        for node in children(current): 
+            openset.add(node)
+            node.parent = current
+            
 
-    #Guardar en una lista/dict los estudiantes problematicos ya asignados y su asiento
-    raise ValueError('No Path Found')
-
-
-if __name__ == "__main__":
-
-    with open('easytest.prob') as f:
+if __name__ == '__main__':
+    start = time.time()
+    '''with open(sys.argv[1], 'r') as f:
+        data = f.read()'''
+    with open('students.prob', 'r') as f:
         data = f.read()
     # reconstructing the data as a dictionary
     students = ast.literal_eval(data)
-    result = aStar(addStudents(students))
-    print(result)
+    open, closed, result = astar(getStudents(students))
+    end = time.time()
+
+    res = {}
+    for s in result.state:
+        res[s.id + ('C' if s.tr else 'X') + ('R' if s.rm else 'X')] = s.seat
+
+    fname = "{}-{}".format(os.path.splitext(sys.argv[1])[0], sys.argv[2]) 
+    with open(fname + '.output', 'w') as output:
+        output.write("INITIAL:\t{} \nFINAL:\t{}".format(data, res))
+
+    with open(fname + '.stat', 'w') as stat:
+        stat.write("Total time: {}\nPlan cost: {}\nExpanded nodes: {}\nPlan length: {}", end-start, result.cost, len(open), len(closed))
